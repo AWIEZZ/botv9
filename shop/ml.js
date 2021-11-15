2c964a19ad8f35491fe22757472c328fb9edc1c6
AWIEZZ <93357097+AWIEZZ@users.noreply.github.com>
2021-11-15 19:28:02 +0700

    Add files via upload

---
+++ shop/ml.js
@@ -0,0 +1,24 @@
+const list_ml = (prefix) => {
+return `*ZEEONE OFC さ*
+			
+PROSES	1-60Mnt Max 1X24 Jam		
+OPEN	08.00-20.00		
+FORMAT	ID		
+			
+DIAMOND MOBILE LEGENDS
+		
+86DIAMOND	  Rp 20,000.00		
+172DIAMOND	  Rp 39,000.00		
+257DIAMOND   Rp 61,500.00		
+344DIAMOND   Rp 83,500.00		
+429DIAMOND   Rp 102,500.00		
+514DIAMOND   Rp 129,000.00		
+706DIAMOND 	Rp 167,500.00		
+878DIAMOND 	Rp 212,000.00		
+1.220DIAMOND	 Rp 230,000.00		
+
+Untuk pay / pembayan silahkan ketik ${prefix}pay
+`
+	}
+
+exports.list_ml = list_ml
